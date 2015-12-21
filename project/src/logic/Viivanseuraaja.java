63b0466475923d9e09a9df8360cb8ef56623a1e3
TheDuckFIN <valtteri.lakaniemi@gmail.com>
2015-12-22 01:20:32 +0200

    Escape-buttonhöskä alku

--- project/src/logic/Viivanseuraaja.java
+++ project/src/logic/Viivanseuraaja.java
@@ -1,5 +1,9 @@
 package logic;
 
 public class Viivanseuraaja {
-
+    
+    public void start() {
+        
+    }
+    
 }
---
+++ project/src/movementhandler/TwoMotorMovement.java
@@ -0,0 +1,17 @@
+package movementhandler;
+
+public class TwoMotorMovement implements MovementHandler {
+
+    public TwoMotorMovement() {
+        
+    }
+    
+    public void move(int amount) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void rotate(int amount) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
