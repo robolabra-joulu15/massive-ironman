d5ed560fc314ad2332ff1f562e21570056a8f230
Live session user <lego@lego.(none)>
2015-12-24 22:51:12 +0200

    DifferentialPilot -testailua.

--- project/src/logic/LineFollower.java
+++ project/src/logic/LineFollower.java
@@ -1,6 +1,8 @@
 package logic;
 
+import lejos.nxt.Button;
 import lejos.nxt.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
 
@@ -8,10 +10,14 @@ public class LineFollower {
     
     public void start() {
     	
+    	DifferentialPilot dp = new DifferentialPilot(5.6f, 12.3f, Motor.A, Motor.B);
+    	Button.ENTER.waitForPress();
+    	dp.rotate(360);
+    	
     	BehaviorHandler behaviors = new BehaviorHandler();
     	
     	behaviors.addBehavior(new ShutdownBehavior(), 0);
-    	behaviors.addBehavior(new MoveforwardBehavior(Motor.A, Motor.B), 4);
+    	//behaviors.addBehavior(new MoveforwardBehavior(Motor.A, Motor.B), 1);
     	
     	Arbitrator arbitrator = new Arbitrator(behaviors.getArray());
     	arbitrator.start();
