82b866ed89aecaa0d9e03cac1aa37dce0c2eeb5f
Live session user <lego@lego.(none)>
2015-12-26 21:25:31 +0200

    Runkoa uudelle toteutukselle

--- project/src/logic/LineFollower.java
+++ project/src/logic/LineFollower.java
@@ -1,8 +1,18 @@
 package logic;
 
+import ui.ConfiguratorUI;
+
 public class LineFollower {
 
 	public void start() {
+		
+		ConfiguratorUI config = new ConfiguratorUI();
+		
+		while(true) {
+			
+			if (!config.start()) break;
+			
+		}
 		
 	}
 	
---
+++ project/src/ui/ConfiguratorUI.java
@@ -0,0 +1,19 @@
+package ui;
+
+import lejos.nxt.Button;
+
+public class ConfiguratorUI {
+
+	public boolean start() {
+		
+		while(!Button.ESCAPE.isPressed()) {
+			
+			
+			
+		}
+		
+		return false;
+		
+	}
+	
+}
