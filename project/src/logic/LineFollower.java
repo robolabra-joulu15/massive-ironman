961b8861fa3611b3a89ee335070bdad5a7d69dca
Live session user <lego@lego.(none)>
2015-12-26 21:18:08 +0200

    Ison muutoksen aika. Vanhat sourcet siirretty old_src kansioon ja aloitan nyt projektin toteutuksen ihan eri näkökulmasta. Lisää tiedostossa plans/26_12_2015.md

--- project/src/logic/LineFollower.java
+++ project/src/logic/LineFollower.java
@@ -1,38 +1,9 @@
 package logic;
 
-import lejos.nxt.Button;
-import lejos.nxt.LCD;
-import lejos.nxt.LightSensor;
-import lejos.nxt.Motor;
-import lejos.nxt.SensorPort;
-import lejos.robotics.navigation.DifferentialPilot;
-import lejos.robotics.subsumption.Arbitrator;
-import lejos.robotics.subsumption.Behavior;
-
 public class LineFollower {
-    
-    public void start() {
-    	
-    	DifferentialPilot pilot = new DifferentialPilot(5.6f, 12.0f, Motor.A, Motor.B);
-    	pilot.setRotateSpeed(180);
-    	pilot.setTravelSpeed(2);
-    	
-    	LightSensor light = new LightSensor(SensorPort.S1);
-    	light.setFloodlight(true);
-    	
-    	BehaviorHandler behaviors = new BehaviorHandler();
-    	
-    	behaviors.addBehavior(new ShutdownBehavior(), 0);
-    	behaviors.addBehavior(new MoveforwardBehavior(pilot, light), 2);
-    	behaviors.addBehavior(new RotateBehavior(pilot, light), 1);
-    	
-    	Arbitrator arbitrator = new Arbitrator(behaviors.getArray());
-    	
-    	Button.ENTER.waitForPress();
-    	
-    	SensorPort.S1.addSensorPortListener(new LCDUpdater(light));
-    	arbitrator.start();
-    
-    }
-    
+
+	public void start() {
+		
+	}
+	
 }
