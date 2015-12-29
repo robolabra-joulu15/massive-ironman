774c591f19c07be91d5e6912fc15ff9431eeb1c2
Live session user <lego@lego.(none)>
2015-12-29 20:14:29 +0200

    FloodLight säätöä LightValueSelectroriin ja PIDController jatkuu

--- project/src/logic/PIDController.java
+++ project/src/logic/PIDController.java
@@ -1,5 +1,6 @@
 package logic;
 
+import lejos.nxt.Button;
 import lejos.nxt.LightSensor;
 import util.Configuration;
 
@@ -12,5 +13,22 @@ public class PIDController {
 	    this.config = config;
 	    this.light = light;
     }
+	
+	public void start() {
+		//the average of background and line light values
+		int offset = (this.config.getBackgroundColor() - this.config.getLineColor()) / 2;
+		//target power level (the speed of both motors when going straight forward)
+		int targetPower = this.config.getMovementSpeed();
+		//Proportional constant
+		int kp = targetPower / (this.config.getBackgroundColor() - offset);
+		
+		this.light.setFloodlight(true);
+		
+		while(!Button.ESCAPE.isPressed()) {
+			int lightValue = this.light.readValue();
+		}
+		
+		this.light.setFloodlight(false);
+	}
 	
 }
--- project/src/ui/LightValueSelector.java
+++ project/src/ui/LightValueSelector.java
@@ -12,17 +12,19 @@ public class LightValueSelector {
 	private String description;
 	
 	public LightValueSelector(String description) {
-		this.light = new LightSensor(SensorPort.S1, true);
+		this.light = new LightSensor(SensorPort.S1);
 		this.description = description;
 	}
 	
 	public int select() {
+		this.light.setFloodlight(true);
 		LCD.drawString("Press any key", 2, 1);
 		LCD.drawString("to select light", 1, 2);
 		LCD.drawString("value for", 4, 3);
 		LCD.drawString("\"" + this.description + "\"", (16-this.description.length())/2, 5);
 		Button.waitForPress();
 		
+		this.light.setFloodlight(false);
 		return this.light.readValue();	
 	}
 	
