9dd912f7ea785c08c29ad0b0a7fdf0b1ae50e55f
joopeti <pyry.joona@helsinki.fi>
2016-01-10 23:26:50 +0200

    lisää kommentteja

--- roboprojekti/src/org/lejos/soittaja/Utils.java
+++ roboprojekti/src/org/lejos/soittaja/Utils.java
@@ -14,32 +14,32 @@ public class Utils {
 	//listen for four claps and return the average interval between them.
 	public static int getTempo(){
 		SensorPort sp = SensorPort.S1;
-        SoundSensor sensor = new SoundSensor(sp, true);
-        int claps = 0;
-        long last = 0;
-        int clap_intervals = 0;
+    SoundSensor sensor = new SoundSensor(sp, true);
+    int claps = 0;
+    long last = 0;
+    int clap_intervals = 0;
 
-        while(claps < 4){
-        	LCD.drawString("Kuunnellaan tempoa: " + claps, 0, 2);
-        	int vol = sensor.readValue();
+    while(claps < 4){
+    	LCD.drawString("Kuunnellaan tempoa: " + claps, 0, 2);
+    	int vol = sensor.readValue();
 
-        	if(vol > 30){
-        		long clap = System.currentTimeMillis();
-        		if(last != 0){
-        			clap_intervals += (clap - last);
-        		}
-        		last = clap;
-        		claps++;
-        		LCD.drawString("tempo: " + clap_intervals, 0, 2);
-        		LCD.drawString("clap: " + claps, 0, 4);
-        		//wait the sound to decrease before listening for the next clap
-        		Delay.msDelay(200);
-        	}
-        	Delay.msDelay(20);
-        }
-
+    	if(vol > 30){
+    		long clap = System.currentTimeMillis();
+    		if(last != 0){
+    			clap_intervals += (clap - last);
+    		}
+    		last = clap;
+    		claps++;
+    		LCD.drawString("tempo: " + clap_intervals, 0, 2);
+    		LCD.drawString("clap: " + claps, 0, 4);
+    		//wait the sound to decrease before listening for the next clap
+    		Delay.msDelay(200);
+    	}
+    	Delay.msDelay(20);
+    }
+		
 		return clap_intervals / 3;
-	}
+}
 
 	public static void resetMotors(){
 		Motor.A.rotateTo(0);
